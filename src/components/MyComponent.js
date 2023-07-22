8fcaf8460b1896e61d707498b2fa41a1659c36b3
Le Quang Phuc <phuclqgcs220005@fpt.edu.vn>
2023-07-22 11:05:58 +0700

    DOMevents

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,8 +1,24 @@
 import React from 'react'
 class MyComponent extends React.Component {
+    state = {
+        name: 'rac',
+        address: 'bai rac'
+    }
+    handleClick() {
+        console.log('I was clicked')
+    }
+    handleHover() {
+        console.log('mouse over')
+    }
     render() {
         return (
-            <div> Hello n Mycomponent</div>
+            <>
+                <div> Hello n Mycomponent</div>
+                <button onClick={this.handleClick}>Click me</button>
+                <button onMouseOver={this.handleHover}>Hover me</button>
+
+            </>
+
         )
     }
 }
