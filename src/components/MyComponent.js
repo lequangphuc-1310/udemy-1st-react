754ffba16f2e18cb88c6a71fdab0e8ac650323a3
Le Quang Phuc <phuclqgcs220005@fpt.edu.vn>
2023-07-22 11:21:35 +0700

    setState

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -2,10 +2,18 @@ import React from 'react'
 class MyComponent extends React.Component {
     state = {
         name: 'rac',
-        address: 'bai rac'
+        address: 'bai rac',
+        age: 18
     }
-    handleClick() {
-        console.log('I was clicked')
+    handleClick = (e) => {
+        const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+
+        const randomCharacter = alphabet[Math.floor(Math.random() * alphabet.length)]
+        this.setState({
+            name: randomCharacter,
+            age: Math.floor(Math.random() * 100)
+        })
     }
     handleHover() {
         console.log('mouse over')
@@ -13,8 +21,8 @@ class MyComponent extends React.Component {
     render() {
         return (
             <>
-                <div> Hello n Mycomponent</div>
-                <button onClick={this.handleClick}>Click me</button>
+                <div> Hello {this.state.name}, your age is {this.state.age}</div>
+                <button onClick={e => { this.handleClick(e) }}>Click me</button>
                 <button onMouseOver={this.handleHover}>Hover me</button>
 
             </>
