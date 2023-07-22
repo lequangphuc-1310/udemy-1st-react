0670421b54a49212f77f611108a8046c94e23e9c
Le Quang Phuc <phuclqgcs220005@fpt.edu.vn>
2023-07-22 09:57:57 +0700

    video16

---
+++ src/components/MyComponent.js
@@ -0,0 +1,10 @@
+import React from 'react'
+class MyComponent extends React.Component {
+    render() {
+        return (
+            <div> Hello n Mycomponent</div>
+        )
+    }
+}
+
+export default MyComponent
