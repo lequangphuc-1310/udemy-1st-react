143a96a12ed922281e9beaee1ed54d87bc8f87b2
Le Quang Phuc <phuclqgcs220005@fpt.edu.vn>
2023-07-22 11:38:55 +0700

    Form with react

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -7,24 +7,28 @@ class MyComponent extends React.Component {
     }
     handleClick = (e) => {
         const alphabet = "abcdefghijklmnopqrstuvwxyz"
-
-
         const randomCharacter = alphabet[Math.floor(Math.random() * alphabet.length)]
         this.setState({
             name: randomCharacter,
             age: Math.floor(Math.random() * 100)
         })
     }
-    handleHover() {
-        console.log('mouse over')
+    handleOnChangeInput = e => {
+        this.setState({ name: e.target.value })
+    }
+    handleSubmit = e => {
+        e.preventDefault()
+        console.log(this.state)
     }
     render() {
         return (
             <>
                 <div> Hello {this.state.name}, your age is {this.state.age}</div>
                 <button onClick={e => { this.handleClick(e) }}>Click me</button>
-                <button onMouseOver={this.handleHover}>Hover me</button>
-
+                <form onClick={e => { this.handleSubmit(e) }}>
+                    <input type='text' value={this.state.name} onChange={(e) => { this.handleOnChangeInput(e) }} />
+                    <button >Submit</button>
+                </form>
             </>
 
         )
