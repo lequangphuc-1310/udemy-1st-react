82aa869a6d3916652b11bfbca81f14cd3c0e1ef6
Le Quang Phuc <phuclqgcs220005@fpt.edu.vn>
2023-07-23 22:30:21 +0700

    convert class component to hook component

--- src/components/AddUserInfo.js
+++ src/components/AddUserInfo.js
@@ -1,39 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-class AddUserInfo extends React.Component {
-    state = {
+const AddUserInfo = (props) => {
+    const [state, setState] = useState({
         name: '',
-        address: 'bai rac',
         age: ''
+    })
+
+    const [name, setName] = useState('')
+    const [age, setAge] = useState('')
+
+    const handleOnChangeInput = e => {
+        setName(e.target.value)
     }
-    handleOnChangeInput = e => {
-        this.setState({ name: e.target.value })
+
+    const handleOnChangeAge = e => {
+        setAge(e.target.value)
     }
-    handleSubmit = e => {
+
+    const handleSubmit = e => {
         e.preventDefault()
-        this.props.handleAddUser({
+        props.handleAddUser({
             id: Math.floor(Math.random() * 100 + 1) + '-random',
-            name: this.state.name,
-            age: this.state.age
+            name: name,
+            age: age
         })
 
     }
-    handleOnChangeAge = e => {
-        this.setState({ age: e.target.value })
-    }
-    render() {
-        return (
-            <>
-                <form >
-                    <label>Enter ur name: </label>
-                    <input type='text' value={this.state.name} onChange={(e) => { this.handleOnChangeInput(e) }} /><br />
-                    <label>Enter ur age: </label>
-                    <input type='text' value={this.state.age} onChange={(e) => { this.handleOnChangeAge(e) }} /><br />
-                    <button onClick={e => { this.handleSubmit(e) }}>Submit</button>
-                </form>
-            </>
-        )
-    }
+
+    return (
+        <>
+            <form >
+                <label>Enter ur name: </label>
+                <input type='text' value={name} onChange={(e) => { handleOnChangeInput(e) }} /><br />
+                <label>Enter ur age: </label>
+                <input type='text' value={age} onChange={(e) => { handleOnChangeAge(e) }} /><br />
+                <button onClick={e => { handleSubmit(e) }}>Submit</button>
+            </form>
+        </>
+    )
 }
 
 export default AddUserInfo
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,41 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import AddUserInfo from './AddUserInfo'
 import DisplayInfo from './DisplayInfo'
-class MyComponent extends React.Component {
-    state = {
-        listUser: [
-            { id: 1, name: 'okokok', age: 21 },
-            { id: 2, name: 'rac', age: 11 },
-            { id: 3, name: 'ahehe', age: 22 },
-        ]
+const MyComponent = () => {
+    const [listUser, setListUser] = useState([
+        { id: 1, name: 'okokok', age: 21 },
+        { id: 2, name: 'rac', age: 11 },
+        { id: 3, name: 'ahehe', age: 22 },
+    ]
+    )
+    const handleAddUser = (data) => {
+        setListUser([data, ...listUser])
     }
-    handleAddUser = (data) => {
-        this.setState({
-            listUser: [data, ...this.state.listUser]
-        })
-    }
-    handleDeleteUser = (id) => {
-        let cloneListUser = [...this.state.listUser]
+    const handleDeleteUser = (id) => {
+        let cloneListUser = [...listUser]
         cloneListUser = cloneListUser.filter(item => item.id !== id)
-        this.setState({
-            listUser: cloneListUser
-        })
-    }
-    render() {
-
-        return (
-            <>
-                <div className='a'>
-                    <AddUserInfo
-                        handleAddUser={this.handleAddUser}
-                    />
-                    <DisplayInfo listUser={this.state.listUser} handleDeleteUser={this.handleDeleteUser}
-                    />
-                </div>
-                <div className='b'></div>
-            </>
+        setListUser(cloneListUser
         )
     }
+    return (
+        <>
+            <div className='a'>
+                <AddUserInfo
+                    handleAddUser={handleAddUser}
+                />
+                <DisplayInfo listUser={listUser} handleDeleteUser={handleDeleteUser}
+                />
+            </div>
+            <div className='b'></div>
+        </>
+    )
 }
 
 export default MyComponent
